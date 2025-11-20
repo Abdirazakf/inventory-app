ba53d363d245470f7c0db7d6da21fb3e613a1cf7
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-20 18:10:35 -0500

    Added game route controller with get all and search query methods

---
+++ backend/controllers/gameController.js
@@ -0,0 +1,37 @@
+const db = require('../db/queries')
+const {query, validationResult} = require('express-validator')
+
+const lengthErr = 'Search must be between 1 and 255 characters'
+
+const validateSearch = [
+    query('q').trim()
+    .isLength({min: 1, max: 255})
+    .withMessage(lengthErr)
+]
+
+exports.gameListGet =  async (req, res) => {
+    const games = await db.getAllGames()
+    res.send(games)
+}
+
+exports.gameSearchGet = [
+    validateSearch,
+    async (req, res) => {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            })
+        }
+
+        const searchedGame = req.query.q || ""
+
+        if (!searchedGame) {
+            return res.json([])
+        }
+
+        const result = await db.getSearchedGame(searchedGame)
+        res.json(result)
+    }
+]
