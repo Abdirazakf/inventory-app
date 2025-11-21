b918897d81b66d66eee8db8ff187f6c24fadce2a
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-21 01:00:43 -0500

    Established connection to render database with ssl

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -2,5 +2,11 @@ const {Pool} = require('pg')
 require('dotenv').config()
 
 module.exports = new Pool({
-    connectionString: process.env.DB_STRING
+    connectionString: process.env.DB_STRING,
+    ssl: {
+        rejectUnauthorized: false,
+        sslmode: 'require'
+    },
+    connectionTimeoutMillis: 10000,
+    idleTimeoutMillis: 30000
 })
