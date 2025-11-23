1c0536476193f74cb27cc75fe5a60ce52c531cfe
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-23 14:58:38 -0500

    Deployment bug fix

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')))
     
     // Handle React routing - send all non-API requests to index.html
-    app.get('/*', (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
     })
 }
