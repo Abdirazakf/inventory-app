13b285ec3ef270e039d20b09ecb54ff26110ea33
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-23 01:00:42 -0500

    Set the data theme for the App to use global theme state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@ import {Routes, Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Games from "./pages/Games"
 import Homepage from "./pages/Homepage"
+import { useThemeStore } from "./store/useThemeStore"
 
 export default function App() {
+    const {currentTheme} = useThemeStore()
+
     return (
-        <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme="forest">
+        <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={currentTheme}>
             <Navbar />
             <Routes>
                 <Route path='/' element={<Homepage/>} />
