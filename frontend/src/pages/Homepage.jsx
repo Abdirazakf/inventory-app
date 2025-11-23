f7132665cf3d50c6d494e928ac42bce4803a6de0
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-23 16:21:59 -0500

    Check for loading state when fetching from api and then displaying each game with card component

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react"
 import { useGameStore } from "../states/useGameStore"
+import {PlusCircleIcon, RefreshCwIcon} from "lucide-react"
+import GameCard from '../components/GameCard'
 
 export default function Homepage(){
-    const {gameList, fetchGames} = useGameStore()
+    const {gameList, loading, error, fetchGames} = useGameStore()
 
     useEffect(() => {
         fetchGames()
@@ -11,6 +13,28 @@ export default function Homepage(){
     console.log(gameList)
     
     return(
-        <div>Homepage</div>
+        <main className="max-w-6xl mx-auto px-4 py-8">
+            <div className="flex justify-between items-center mb-8">
+                <button className="btn btn-primary">
+                    <PlusCircleIcon className="size-5 mr-2" />
+                    Add Game
+                </button>
+                <button className="btn btn-ghost btn-circle" onClick={fetchGames}>
+                    <RefreshCwIcon className="size-5"/>
+                </button>
+            </div>
+
+            {loading ? (
+                <div className="flex justify-center items-center h-64">
+                    <div className="loading loading-spinner loading-lg"/>
+                </div>
+            ) : (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {gameList.map(game => (
+                        <GameCard key={game.id} game={game} />
+                    ))}
+                </div>
+            )}
+        </main>
     )
 }
