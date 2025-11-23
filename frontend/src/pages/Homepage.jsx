82dd311487ae657885f697bcc3487803cbfef8b4
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-23 14:40:51 -0500

    Test fetching from the backend to console log the game list from the database

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+import { useGameStore } from "../states/useGameStore"
+
 export default function Homepage(){
+    const {gameList, fetchGames} = useGameStore()
+
+    useEffect(() => {
+        fetchGames()
+    }, [fetchGames])
+
+    console.log(gameList)
+    
     return(
         <div>Homepage</div>
     )
