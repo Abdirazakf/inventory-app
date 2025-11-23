bca4f4f6e7ba49fdd1cddeecbc9b78b415e450bd
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-23 16:38:05 -0500

    Created card with game image, title, price, and edit button

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -1,5 +1,29 @@
+import { EditIcon } from "lucide-react"
+import {Link} from "react-router-dom"
+
 export default function GameCard({game}) {
     return (
-        <div>{game.title}</div>
+        <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
+            <figure className="relative pt-[56.25%">
+                <img src={game.image_url} alt={game.title}
+                className="absolute top-0 left-0 w-full h-full object-cover"
+                />
+            </figure>
+
+            <div className="card-body">
+                <h2 className="card-title text-lg font-semibold">{game.title}</h2>
+                {game.price && (
+                    <p className="text-2xl font-bold text-primary">
+                        {game.price > 0 ? `${game.price}` : 'Free'}
+                    </p>
+                )}
+
+                <div className="card-actions justify-end mt-4">
+                    <Link to={`/games/${game.id}`} className="btn btn-sm btn-info btn-outline">
+                        <EditIcon className="size-4" />
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
