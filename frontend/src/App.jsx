2a64cbb2f13415fd82c45b169c28bca1b8591561
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-22 23:26:17 -0500

    Added routes and pages to App and main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import {Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Games from "./pages/Games"
+import Homepage from "./pages/Homepage"
+
 export default function App() {
     return (
-        <div className="text-red-400">App</div>
+        <div className="min-h-screen bg-base-200 transition-colors duration-300">
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Homepage/>} />
+                <Route path='/:id' element={<Games/>} />
+            </Routes>
+        </div>
     )
 }
