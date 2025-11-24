2d1e067ead9e228901a78d98f860a00c3b394a0e
Abdirazakf <farahabdirazak13@gmail.com>
2025-11-23 20:39:39 -0500

    Fixed bug with the bottom border of the nav bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar(){
     const {pathname} = useResolvedPath()
     const isHomepage = pathname === "/"
 
-    return( <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-conten/10 sticky top-0 z-50">
+    return( <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
         <div className="max-w-7xl mx-auto">
             <div className="navbar px-4 min-h-[4rem] justify-between">
                 <div className="flex-1 lg:flex-none">
